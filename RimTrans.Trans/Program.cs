f551bbca9d24ada436b18480510a3cdb86d443ea
duduluu@outlook.com <duduluu@outlook.com>
2017-03-30 18:54:59 +0800

    Bug fix and Add Special Mode

--- RimTrans.Trans/Program.cs
+++ RimTrans.Trans/Program.cs
@@ -170,12 +170,12 @@ namespace RimTrans.Trans
             if (generateOption == "Standard" &&
                 (string.IsNullOrWhiteSpace(corePath) || !Directory.Exists(corePath)))
             {
-                generateOption = "Special";
+                generateOption = "Core";
                 Log.Warning();
                 Log.WriteLine(ConsoleColor.Yellow, "Core Directory NO FOUND.");
                 Log.Indent();
                 Log.Write(ConsoleColor.Yellow, "Changed Generate Option to ");
-                Log.WriteLine(ConsoleColor.Cyan, "Special Mode");
+                Log.WriteLine(ConsoleColor.Cyan, "Core Mode");
             }
 
             if (generateOption == "Standard")
@@ -191,7 +191,7 @@ namespace RimTrans.Trans
 
             #region Process Core and Special Mode
 
-            if (generateOption == "Core" || generateOption == "Special")
+            if (generateOption == "Core")
             {
 
                 Log.WriteLine(ConsoleColor.Green, "======== Start Processing Defs and Original Language Data ========");
@@ -274,7 +274,7 @@ namespace RimTrans.Trans
 
             #region Process Standard Mode
 
-            if (generateOption == "Standard")
+            else if (generateOption == "Standard")
             {
 
                 Log.WriteLine(ConsoleColor.Green, "======== Start Processing Core Defs and Original Language Data ========");
@@ -288,6 +288,122 @@ namespace RimTrans.Trans
                 Log.WriteLine(ConsoleColor.Green, "======== Completed Processing Core Defs and Original Language Data ========");
                 Log.WriteLine();
                 
+                Log.WriteLine(ConsoleColor.Green, "======== Start Processing Mod Defs and Original Language Data ========");
+                string defsPath = Path.Combine(modPath, "Defs");
+                string keyedPath_English = Path.Combine(modPath, "Languages", "English", "Keyed");
+                string stringsPath_English = Path.Combine(modPath, "Languages", "English", "Strings");
+
+                DefinitionData Defs = DefinitionData.Load(defsPath, Core_Defs);
+                InjectionData DefInjected_Original = InjectionData.Parse(Defs);
+                KeyedData Keyed_English = KeyedData.Load(keyedPath_English);
+                Log.WriteLine(ConsoleColor.Green, "======== Completed Processing Mod Defs and Original Language Data ========");
+                Log.WriteLine();
+
+                XElement Languages = root.Element("Languages");
+                foreach (XElement li in Languages.Elements())
+                {
+                    // If IsChecked
+                    if (string.Compare(li.Element("IsChecked").Value, "false", true) == 0)
+                        continue;
+
+                    string realName = li.Element("RealName").Value;
+                    string nativeName = li.Element("NativeName").Value;
+
+                    // Check Language Real Name
+                    if (string.IsNullOrWhiteSpace(realName))
+                    {
+                        Log.Error();
+                        Log.WriteLine(ConsoleColor.Red, "Missing Language Name.");
+                        continue;
+                    }
+
+                    Log.WriteLine(ConsoleColor.Green, $"======== Start Processing Language: {realName} ( {nativeName} ) ========");
+
+                    string langPath = Path.Combine(modPath, "Languages", realName);
+                    bool isCustom = (string.Compare(li.Element("IsCustom").Value, "true", true) == 0);
+                    if (isCustom)
+                    {
+                        langPath = li.Element("CustomPath").Value;
+                        Log.WriteLine(ConsoleColor.Cyan, "Use Custom Language Output Directory: ");
+                    }
+                    else
+                    {
+                        Log.WriteLine(ConsoleColor.Cyan, "Language Path: ");
+                    }
+                    Log.Indent();
+                    Log.WriteLine(ConsoleColor.Cyan, langPath);
+
+                    // Check Custom Path
+                    if (string.IsNullOrWhiteSpace(langPath))
+                    {
+                        Log.Error();
+                        Log.WriteLine(ConsoleColor.Red, "Invalid Custom Output Directory Path.");
+                        continue;
+                    }
+
+                    string core_defInjectedPath = Path.Combine(core_langPath, realName, "DefInjected");
+                    string core_keyedPath = Path.Combine(core_langPath, realName, "Keyed");
+
+                    //InjectionData Core_DefInjected_New = new InjectionData(Core_DefInjected_Original);
+                    //InjectionData Core_DefInjected_Existed = InjectionData.Load(core_defInjectedPath);
+                    //Core_DefInjected_New.MatchExisted(Core_DefInjected_Existed);
+
+                    //KeyedData Core_Keyed_New = new KeyedData(Core_Keyed_English);
+                    //KeyedData Core_Keyed_Existed = KeyedData.Load(core_keyedPath);
+                    //Core_Keyed_New.MatchExisted(Core_Keyed_Existed);
+
+                    string defInjectedPath = Path.Combine(langPath, "DefInjected");
+                    string keyedPath = Path.Combine(langPath, "Keyed");
+                    string stringsPath = Path.Combine(langPath, "Strings");
+
+                    InjectionData DefInjected_New = new InjectionData(DefInjected_Original);
+                    //DefInjected_New.MatchCore(Core_DefInjected_New);
+
+                    KeyedData Keyed_New = new KeyedData(Keyed_English);
+                    //Keyed_New.MatchCore(Core_Keyed_New);
+
+                    if (cleanModeOn)
+                    {
+                        DirectoryHelper.CleanDirectory(defInjectedPath, "*.xml");
+                        DirectoryHelper.CleanDirectory(keyedPath, "*.xml");
+                        DirectoryHelper.CleanDirectory(stringsPath, "*.txt");
+                    }
+                    else
+                    {
+                        InjectionData DefInjected_Existed = InjectionData.Load(defInjectedPath, true);
+                        DefInjected_New.MatchExisted(DefInjected_Existed);
+
+                        KeyedData Keyed_Existed = KeyedData.Load(keyedPath, true);
+                        Keyed_New.MatchExisted(Keyed_Existed);
+                    }
+
+                    DefInjected_New.Save(defInjectedPath);
+                    Keyed_New.Save(keyedPath);
+                    DirectoryHelper.CopyDirectoryEx(stringsPath_English, stringsPath, "*.txt");
+
+                    Log.WriteLine(ConsoleColor.Green, $"======== Completed Processing Language: {realName} ( {nativeName} ) ========");
+                    Log.WriteLine();
+                }
+            }
+
+            #endregion
+
+            #region Special Mode
+
+            else if (generateOption == "Special")
+            {
+
+                Log.WriteLine(ConsoleColor.Green, "======== Start Processing Core Defs and Original Language Data ========");
+                string core_defsPath = Path.Combine(corePath, "Defs");
+                string core_langPath = Path.Combine(corePath, "Languages");
+                string core_keyedPath_English = Path.Combine(core_langPath, "English", "Keyed");
+
+                DefinitionData Core_Defs = DefinitionData.Load(core_defsPath);
+                InjectionData Core_DefInjected_Original = InjectionData.Parse(Core_Defs);
+                KeyedData Core_Keyed_English = KeyedData.Load(core_keyedPath_English);
+                Log.WriteLine(ConsoleColor.Green, "======== Completed Processing Core Defs and Original Language Data ========");
+                Log.WriteLine();
+
                 Log.WriteLine(ConsoleColor.Green, "======== Start Processing Mod Defs and Original Language Data ========");
                 string defsPath = Path.Combine(modPath, "Defs");
                 string keyedPath_English = Path.Combine(modPath, "Languages", "English", "Keyed");
