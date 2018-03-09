68176268fa3c1c95a05cd15e8ac3bf2a377ddb52
duduluu <duduluu@outlook.com>
2018-03-10 02:37:50 +0800

    Update Test

---
+++ Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using duduluu.System.Linq;
+
+using RimTrans.Core;
+
+namespace Test {
+    class Program {
+        static void Main(string[] args) {
+            Test(TestLoadMods);
+        }
+
+        static void Test(Action testFunc, int frequency = 32) {
+            var stopwatch = new Stopwatch();
+            var totalTime = 0.0;
+
+            for (int i = 0; i < frequency; i++) {
+                stopwatch.Restart();
+                testFunc();
+                stopwatch.Stop();
+                Console.WriteLine($"[# {i}] Elapsed time for {testFunc.Method.Name}: {stopwatch.Elapsed.TotalMilliseconds} ms");
+                totalTime += stopwatch.Elapsed.TotalMilliseconds;
+            }
+
+            Console.WriteLine($"[Total] Elapsed time for {testFunc.Method.Name}: Total: {totalTime} ms, Average: {totalTime / frequency} ms");
+        }
+
+        static void TestLoadMods() {
+            Directory.GetDirectories(@"D:\rw\test")
+                .ForEach(dir => {
+                    var mod = new Mod(dir);
+                    mod.LoadDefsAsync().Wait();
+                });
+        }
+    }
+}
