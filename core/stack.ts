816c571c2791529ffcced8405bcedfab472fc686
duduluu <duduluu@outlook.com>
2018-06-30 02:27:23 +0800

    Update core/stack.ts

--- core/stack.ts
+++ core/stack.ts
@@ -1,35 +1,39 @@
 /**
- * Simple Stack
+ * Stack
  */
-// tslint:disable-next-line:no-any
+// tslint:disable:no-any
 export default class Stack<T = any> {
   private items: T[] = [];
 
+  private top: number = 0;
+
+  public get data(): T[] {
+    return this.items.slice(0, this.top);
+  }
+
   public push(item: T): this {
-    this.items.push(item);
+    this.items[this.top] = item;
+    this.top++;
 
     return this;
   }
 
   public pop(): this {
-    this.items.pop();
+    this.top--;
 
     return this;
   }
 
   public get peek(): T {
-    return this.items[this.items.length - 1];
+    return this.items[this.top - 1];
   }
 
-  public get isEmpty(): boolean {
-    return this.items.length === 0;
-  }
-
-  public get size(): number {
-    return this.items.length;
+  public get length(): number {
+    return this.top;
   }
 
   public clear(): this {
+    this.top = 0;
     this.items = [];
 
     return this;
