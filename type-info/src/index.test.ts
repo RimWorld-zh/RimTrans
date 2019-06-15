442014db00e9dd14baf2f18fcd9809f8a0077b7f
duduluu <duduluu@outlook.com>
2019-06-15 15:44:16 +0800

    remove useless type-info project

--- type-info/src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { getCoreTypeInfo } from './index';
-
-describe('type-info', () => {
-  test('test getCoreTypeInfo', async () => {
-    const { classes } = await getCoreTypeInfo();
-    expect(classes.filter(c => c.baseClass === 'Def').length).toBe(115);
-  });
-});
