f1a88ecfd31de79fe6036f415c4ceeebdf3c88f0
duduluu <duduluu@outlook.com>
2019-06-15 20:16:13 +0800

    remove useless dependencies

--- extractor/src/index.ts
+++ extractor/src/index.ts
@@ -1,9 +1,5 @@
 import jsdom from 'jsdom';
-import { TypeInfoMap, getCoreTypeInfo } from '@rimtrans/type-info';
-import { getCoreFiles } from '@rimtrans/rimworld-core';
 
 export async function extract(): Promise<string> {
-  await getCoreTypeInfo();
-  await getCoreFiles();
   return 'extracted';
 }
